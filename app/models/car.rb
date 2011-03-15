e1e709d2e549df3cd91742565f6d10803206d706
carier <carierboy@gmail.com>
2011-03-15 15:11:27 +0200

    =Addon task commit

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,6 @@
 class Car < ActiveRecord::Base
   has_many :parts, :dependent => :delete_all
+  accepts_nested_attributes_for :parts
 
   validates_presence_of :brand, :model, :year
   validates_numericality_of :year
